dcc89b452999389e63ef51b7f5805c14967a64fe
unknown <piotr.marynczak.programista@gmail.com>
2019-12-08 19:29:47 +0100

    added collapsed menu for edit user profile

--- App/Controllers/Balance.php
+++ App/Controllers/Balance.php
@@ -23,7 +23,20 @@ class Balance extends Authenticated
      */
     public function rangeAction()
     {
-        View::renderTemplate('Balance/range.html');
+         $id = $_SESSION['user_id'];
+         $balance = new Balance_m;
+
+        if (!isset($_POST['month'])) {
+
+            $first_day_of_month = date('Y-m-01');
+            $last_day_of_month = date('Y-m-t');
+
+            $arg['incomes'] = $balance->getIncomes($first_day_of_month, $last_day_of_month, $id);
+            $arg['expenses'] = $balance->getExpenses($first_day_of_month, $last_day_of_month, $id);
+            $arg['pieChart'] = $balance->showBalance($first_day_of_month, $last_day_of_month, $id);
+
+            View::renderTemplate('Balance/range.html', $arg);
+        }
     }
 
     /**
@@ -33,10 +46,11 @@ class Balance extends Authenticated
      */
     public function showAction()
     {
+        $id = $_SESSION['user_id'];
+        $balance = new Balance_m;
 
         if (isset($_POST['month'])) {
 
-            $id = $_SESSION['user_id'];
             $month = $_POST['month'];
 
             if($month == "current_month"){
@@ -53,7 +67,7 @@ class Balance extends Authenticated
                 $first_day_of_month = date(('Y').'-01-01');
                 $last_day_of_month = date('Y') . '-12-31';
             
-            } else if($month == "custom") {
+            } else if($month == "#custom") {
                 
                 $first_day_of_month = $_POST['date1'];
                 $last_day_of_month = $_POST['date2'];
@@ -61,7 +75,6 @@ class Balance extends Authenticated
                 $last_day_of_month = date("Y-m-d", strtotime($last_day_of_month));
             }
 
-            $balance = new Balance_m($_POST);
 
             $arg['incomes'] = $balance->getIncomes($first_day_of_month, $last_day_of_month, $id);
             $arg['expenses'] = $balance->getExpenses($first_day_of_month, $last_day_of_month, $id);
